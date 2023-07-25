39e09c6420102169113a4800af86d82a8ea0627e
Yohana Bazan <yohana.bazan93@gmail.com>
2023-07-25 04:16:47 -0300

    creacion

---
+++ js/tarjeta.js
@@ -0,0 +1,137 @@
+const tarjeta = document.querySelector('#tarjeta'),
+    btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
+    formulario = document.querySelector('#formulario-tarjeta'),
+    numeroTarjeta = document.querySelector('#tarjeta .numero'),
+    nombreTarjeta = document.querySelector('#tarjeta .nombre'),
+    logoMarca = document.querySelector('#logo-marca'),
+    firma = document.querySelector('#tarjeta .firma p'),
+    mesExpiracion = document.querySelector('#tarjeta .mes'),
+    yearExpiracion = document.querySelector('#tarjeta .year');
+ccv = document.querySelector('#tarjeta .ccv');
+
+const mostrarFrente = () => {
+	if(tarjeta.classList.contains('active')){
+		tarjeta.classList.remove('active');
+	}
+}
+
+tarjeta.addEventListener('click', () => {
+    tarjeta.classList.toggle('active');
+});
+
+btnAbrirFormulario.addEventListener('click', () => {
+    btnAbrirFormulario.classList.toggle('active');
+    formulario.classList.toggle('active');
+});
+
+for (let i = 1; i <= 12; i++) {
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.selectMes.appendChild(opcion);
+}
+
+const yearActual = new Date().getFullYear();
+for (let i = yearActual; i <= yearActual + 8; i++) {
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.selectYear.appendChild(opcion);
+}
+
+formulario.inputNumero.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+
+    formulario.inputNumero.value = valorInput
+
+        .replace(/\s/g, '')
+        .replace(/\D/g, '')
+        .replace(/([0-9]{4})/g, '$1 ')
+        .trim();
+
+    numeroTarjeta.textContent = valorInput;
+
+    if (valorInput == '') {
+        numeroTarjeta.textContent = '#### #### #### ####';
+
+        logoMarca.innerHTML = '';
+    }
+
+    if (valorInput[0] == 4) {
+        logoMarca.innerHTML = '';
+        const imagen = document.createElement('img');
+        imagen.src = '../img/visa (1).png';
+        logoMarca.appendChild(imagen);
+    } else if (valorInput[0] == 5) {
+        logoMarca.innerHTML = '';
+        const imagen = document.createElement('img');
+        imagen.src = '../img/mastercard.png';
+        logoMarca.appendChild(imagen);
+    }
+
+
+    mostrarFrente();
+});
+
+
+formulario.inputNombre.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+
+    formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
+    nombreTarjeta.textContent = valorInput;
+    firma.textContent = valorInput;
+
+    if (valorInput == '') {
+        nombreTarjeta.textContent = 'Jhon Doe';
+    }
+
+    mostrarFrente();
+});
+
+
+formulario.selectMes.addEventListener('change', (e) => {
+    mesExpiracion.textContent = e.target.value;
+    mostrarFrente();
+});
+
+formulario.selectYear.addEventListener('change', (e) => {
+    yearExpiracion.textContent = e.target.value.slice(2);
+    mostrarFrente();
+});
+
+formulario.inputCCV.addEventListener('keyup', () => {
+    if (!tarjeta.classList.contains('active')) {
+        tarjeta.classList.toggle('active');
+    }
+
+    formulario.inputCCV.value = formulario.inputCCV.value
+    
+        .replace(/\s/g, '')
+        .replace(/\D/g, '');
+
+    ccv.textContent = formulario.inputCCV.value;
+});
+
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault()
+    if (numeroTarjeta.textContent === '#### #### #### ####' || nombreTarjeta.textContent === 'Nombre' || yearExpiracion.textContent === 'YY' || mesExpiracion.textContent === 'MM' || ccv.textContent === '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debe completar todos los datos!',
+        })
+    } else {
+        Swal.fire({
+            icon: 'success',
+            title: 'Su compra fue realizada con éxito!',
+            text: 'Gracias por elegirnos'
+        }).then(() => {
+            window.location.href = '/index.html'
+            carrito.splice(0, carrito.length)
+            localStorage.setItem("carritoJson", JSON.stringify(carrito));
+            contadorCarrito()
+            pintarCarrito()
+        })
+    }
+})
---
+++ js/tienda.js
@@ -0,0 +1,88 @@
+
+const productos = [
+    {
+        "id": "1",
+        "nombre": "Crop top Mariposa",
+        "precio": 1000,
+        "categoria": "remeras y tops",
+        "img": "../img/remeras - tops/1.png"
+    },
+    {
+        "id": "2",
+        "nombre": "Musculosa Coffee",
+        "precio": 900,
+        "categoria": "remeras y tops",
+        "img": "../img/remeras - tops/3.png"
+    },
+    {
+        "id": "3",
+        "nombre": "Remeron Lovers",
+        "precio": 1200,
+        "categoria": "remeras y tops",
+        "img": "../img/remeras - tops/20.png"
+    },
+    {
+        "id": "4",
+        "nombre": "Buzo Dance",
+        "precio": 1500,
+        "categoria": "buzos",
+        "img": "../img/buzos/5.png"
+    },
+    {
+        "id": "5",
+        "nombre": "Buzo Tumba",
+        "precio": 1500,
+        "categoria": "buzos",
+        "img": "../img/buzos/7.png"
+    },
+    {
+        "id": "6",
+        "nombre": "Buzo Rose",
+        "precio": 1800,
+        "categoria": "buzos",
+        "img": "../img/buzos/10.png"
+    },
+    {
+        "id": "7",
+        "nombre": "Pollera Esotérica",
+        "precio": 1300,
+        "categoria": "faldas y shorts",
+        "img": "../img/faldas-shorts/18.png"
+    },
+    {
+        "id": "8",
+        "nombre": "Short Skull",
+        "precio": 1500,
+        "categoria": "faldas y shorts",
+        "img": "../img/faldas-shorts/26.png"
+    },
+    {
+        "id": "9",
+        "nombre": "Falda Snake",
+        "precio": 1800,
+        "categoria": "faldas y shorts",
+        "img": "../img/faldas-shorts/28.png"
+    },
+    {
+        "id": "10",
+        "nombre": "Pantalón Skull",
+        "precio": 2000,
+        "categoria": "pantalones",
+        "img": "../img/pantalones/12.png"
+    },
+    {
+        "id": "11",
+        "nombre": "Pantalón Disaster",
+        "precio": 2200,
+        "categoria": "pantalones",
+        "img": "../img/pantalones/14.png"
+    },
+    {
+        "id": "12",
+        "nombre": "Pantalón Look at me",
+        "precio": 2500,
+        "categoria": "pantalones",
+        "img": "../img/pantalones/16.png"
+    }
+]
+
